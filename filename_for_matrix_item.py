778b6138b2dd825dba51563534de2223281f8cb2
exquo <62397152+exquo@users.noreply.github.com>
2021-07-18 06:19:54 +0000

    Add workflow and helper scripts

---
+++ filename_for_matrix_item.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+
+matrix = json.loads(sys.argv[1])
+lib_name = sys.argv[2]
+host_runner = sys.argv[3]
+try:
+    cross_target = sys.argv[4]
+except IndexError:
+    cross_target = None
+
+def get_matrix_item(matrix, matrix_key, list_key, list_key_val):
+    matrix_key_list = matrix.get(matrix_key) or []
+    for item in matrix_key_list:
+        if item and item[list_key] == list_key_val:
+            return item
+    for item in matrix["include"]:
+        include_item = item[matrix_key]
+        if include_item[list_key] == list_key_val:
+            return include_item
+    raise ValueError
+
+lib_dict = get_matrix_item(matrix, "lib", "name", lib_name)
+host_dict = get_matrix_item(matrix, "host", "runner", host_runner)
+cross_dict = get_matrix_item(matrix, "cross", "target", cross_target) if cross_target else None
+
+lib_filename = "".join([
+    host_dict.get("lib-prefix") or "",
+    lib_dict["filename"],
+    host_dict["lib-suffix"]
+    ])
+print(lib_filename)
+print("::set-output name=lib_filename::" + lib_filename)
+
+archive_name = "".join([
+    lib_filename,
+    "-",
+    lib_dict["ref"],
+    "-",
+    cross_dict["target"] if cross_dict else host_dict["triple"]
+    ])
+print(archive_name)
+print("::set-output name=archive_name::" + archive_name)
---
+++ generate_matrix.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+import json
+import pprint
+import sys
+
+print(sys.argv)
+zkgroup_version = sys.argv[1]
+libclient_version = sys.argv[2]
+
+hosts = {
+        "linux": {
+                "runner": "ubuntu-18.04",
+                "lib-prefix": "lib",
+                "lib-suffix": ".so",
+                "triple": "x86_64-unknown-linux-gnu",
+                },
+        "macos": {
+                "runner": "macos-latest",
+                "lib-prefix": "lib",
+                "lib-suffix": ".dylib",
+                "triple": "x86_64-apple-darwin",
+            },
+        "windows": {
+                "runner": "windows-latest",
+                "lib-suffix": ".dll",
+                "triple": "x86_64-pc-windows",  # no "-msvc" because static lib, see next line
+                "rust-flags": "-C target-feature=+crt-static",
+                        # Static linking to remove MSVC dependendency.
+                        # zkgroup/ffi/node/Makefile
+                        # libsignal-client/node/build_node_bridge.py
+            },
+        }
+
+cross_targets = [
+        #{
+            #"target": "armv7-unknown-linux-gnueabihf",
+            #"req-pkg": "gcc-arm-linux-gnueabihf",
+            #"linker": "arm-linux-gnueabihf-gcc",
+        #},
+        {
+            "target": "aarch64-unknown-linux-gnu",
+            "req-pkg": "gcc-aarch64-linux-gnu",
+            "linker": "aarch64-linux-gnu-gcc",
+        },
+        #{
+            #"target": "i686-unknown-linux-gnu",
+            #"req-pkg": "gcc-i686-linux-gnu",
+            #"linker": "i686-linux-gnu-gcc",
+        #},
+        ]
+
+
+matrix = {
+        "lib": [
+            {
+                "name": "zkgroup",
+                "repo": "signalapp/zkgroup",
+                "ref": zkgroup_version,
+                "filename": "zkgroup",
+                "cargo-flags": "--verbose",
+            },
+            {
+                "name": "libclient",
+                "repo": "signalapp/libsignal-client",
+                "ref": libclient_version,
+                "filename": "signal_jni",
+                "cargo-flags": "-p libsignal-jni  --verbose",
+            },
+        ],
+        "host": [
+            hosts["linux"],
+            hosts["macos"],
+            hosts["windows"],
+        ],
+        "cross": [None],
+        "include": []
+        }
+
+target_to_host = {"linux": "linux", "apple": "macos"}
+
+for cross in cross_targets:
+    for lib in matrix["lib"]:
+        for target_partial_name, host_name in target_to_host.items():
+            if target_partial_name in cross["target"]:
+                matrix["include"].append(
+                        {
+                            "lib": lib,
+                            "host": hosts[host_name],
+                            "cross": cross,
+                        }
+                    )
+
+if not matrix.get("host"):
+    matrix = {"include": matrix["include"]}
+    jobs_total = len(matrix["include"])
+else:
+    jobs_total = 1
+    for jobs_num in (len(key_array) for key, key_array in matrix.items() if key != "include"):
+        jobs_total *= jobs_num
+    jobs_total += len(matrix["include"])
+print("Total num of jobs:", jobs_total)
+
+pprint.pprint(matrix)
+
+j = json.dumps(matrix)
+print("::set-output name=matrix::" + j)
